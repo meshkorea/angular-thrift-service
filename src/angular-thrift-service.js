b57c87e71d8e9fcb46dc2b6b297b9d1cd323cb25
Hyungsul Kim <hyungsul.kim@gmail.com>
2014-09-10 22:10:10 +0900

    Implement angular-thrift-service

---
+++ src/angular-thrift-service.js
@@ -0,0 +1,47 @@
+(function (window, angular, Thrift, undefined) {
+  'use strict';
+
+  angular.module('angular-thrift-service', [])
+    .service('thriftService', function ($q, $http) {
+      return {
+        create: function (Client, url, timeout) {
+          var transport = new Thrift.Transport('');
+          var protocol = new Thrift.Protocol(transport);
+          var client = new Client(protocol);
+          timeout = timeout || 30000;
+
+          var service = {};
+          var methodNames = [];
+          for (var prop in client) {
+            if (typeof client[prop] === 'function' && prop.substring(0, 5) !== 'send_' && prop.substring(0, 5) !== 'recv_') {
+              methodNames.push(prop);
+            }
+          }
+
+          angular.forEach(methodNames, function (methodName) {
+            service[methodName] = function () {
+              var args = Array.prototype.slice.call(arguments, 0, arguments.length);
+
+              var thriftSend = function (data) {
+                return client['send_' + methodName].apply(client, data);
+              };
+
+              var thriftRecv = function (data) {
+                client.output.transport.setRecvBuffer(data);
+                return client['recv_' + methodName].call(client);
+              };
+
+              return $http.post(url, args, {
+                transformRequest: thriftSend,
+                transformResponse: thriftRecv,
+                timeout: timeout,
+                tracker: methodName});
+            };
+          });
+
+          return service;
+        }
+      };
+    });
+
+})(window, window.angular, window.Thrift);
